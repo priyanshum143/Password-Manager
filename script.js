a691525d8f41f1d2bb3e02a19fc6721cc7abde5b
Priyanshu <mehtapriyanshu408@gmail.com>
2023-10-14 22:43:35 +0530

    Login, signup and change password pages completed

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const emails = ['mehtapriyanshu408@gmail.com', 'priyanshu2349.be21@chitkara.edu.in'];
-const passwords = ['Priyanshu', 'Mehta'];
+const emails = [];
+const passwords = [];
 
+// For Login Form
 document.addEventListener('DOMContentLoaded', function() {
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.querySelector('input[name="password"]');
+    const emailInput = document.getElementById('email1');
+    const passwordInput = document.getElementById('password');
     const loginButton = document.getElementById('login-btn');
 
     loginButton.addEventListener('click', function() {
@@ -13,6 +14,45 @@ document.addEventListener('DOMContentLoaded', function() {
         const idxOfEmail = emails.indexOf(emailValue);
         if(idxOfEmail == -1) alert("You do not have any PassX account. try sign up instead.");
         else if(passwords[idxOfEmail] != passwordValue) alert("Password is wrong, try forgot password");
-        else if(passwords[idxOfEmail] == passwordValue) window.location.href = 'index.html';
+        else if(passwords[idxOfEmail] == passwordValue) window.location.href = 'HomePages/index.html';
+    });
+});
+
+// For Reset Password From
+document.addEventListener('DOMContentLoaded', function() {
+    const emailInput = document.getElementById('email2');
+    const otp = document.getElementById('otp');
+    const passwordInput1 = document.getElementById('new-password');
+    const passwordInput2 = document.getElementById('confirm-password');
+    const resetButton = document.getElementById('changePw-btn');
+
+    resetButton.addEventListener('click', function() {
+        const emailValue = emailInput.value;
+        const passwordVal1 = passwordInput1.value;
+        const passwordVal2 = passwordInput2.value;
+
+        const idxOfEmail = emails.indexOf(emailValue);
+        if(idxOfEmail == -1) alert("Your email does not exist, try sign up instead.");
+        else if(passwordVal1 != passwordVal2) alert("Confirm your password again.");
+        else if(passwordVal1 == passwordVal2){
+            passwords[idxOfEmail] = passwordVal1;
+            window.location.href = 'HomePages/index.html';
+        }
+    });
+});
+
+// For Sign Up From
+document.addEventListener('DOMContentLoaded', function() {
+    const emailInput = document.getElementById('email3');
+    const passwordInput = document.getElementById('set-password');
+    const createButton = document.getElementById('createAccount-btn');
+
+    createButton.addEventListener('click', function() {
+        const emailValue = emailInput.value;
+        const passwordVal = passwordInput.value;
+
+        emails.push(emailValue);
+        passwords.push(passwordVal);
+        window.location.href = 'HomePages/index.html';
     });
 });
