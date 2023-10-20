fdd8d71c2c2f0b8ce252a8a41c7a5548c8c698be
Priyanshu <mehtapriyanshu408@gmail.com>
2023-10-21 02:52:42 +0530

    delete account function added

--- JS_Files/logout.js
+++ JS_Files/logout.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     logoutButton.addEventListener('click', function(){
         localStorage.setItem("isAuthenticated", false);
+        localStorage.setItem("currUser", "");
         window.location.href = "../login.html"
     });
 });
@@ -13,3 +14,20 @@ if(isAuthenticated == "false"){
     const logoutButton = document.getElementById("logoutBtn");
     logoutButton.innerHTML = '<a href="../login.html"> Log In </a>';
 }
+
+// Function to delete account permanently
+document.addEventListener('DOMContentLoaded', function(){
+    const deleteAccButton = document.getElementById('deleteAcc');
+    const currUser = localStorage.getItem("currUser");
+    const dataArr = JSON.parse(localStorage.getItem("userData"));
+
+    deleteAccButton.addEventListener('click', function(){
+        const updatedDataArr = dataArr.filter(function(item){
+            return item.passXUserName != currUser;
+        });
+        localStorage.setItem("userData", JSON.stringify(updatedDataArr));
+        localStorage.setItem("currUser", "");
+        localStorage.setItem("isAuthenticated", false);
+        window.location.href = "../login.html"
+    });
+});
